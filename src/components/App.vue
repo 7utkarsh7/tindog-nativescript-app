e8be983ec4d26ec4e17ab18242ba33e330be3169
7utkarsh7 <utkarshdohare2@gmail.com>
2020-09-20 16:30:21 +0530

    first commit

---
+++ src/components/App.vue
@@ -0,0 +1,60 @@
+<template>
+<Page actionBarHidden="true">
+
+
+            <FlexboxLayout class="page1">
+
+                <StackLayout class="form1">
+                    <Label text="Welcome to " />
+                    <Label text="tindog!" class="header1" />
+
+                    <Button text="Get Started" @tap="btnTap"
+                        class="btn btn-primary btnStart" />
+                </StackLayout>
+            </FlexboxLayout>
+    </Page>
+
+
+
+</template>
+
+<script >
+import Login from "./Login";
+
+  export default {
+      components: {
+          Login
+      },
+      methods: {
+          btnTap(args) {
+              this.$navigateTo(Login);
+          }
+      }
+  };
+</script>
+
+<style scoped>
+.page1 {
+    align-items: center;
+    background-color: #f5f5f5;
+    vertical-align: center;
+    height: 100%;
+    width: 100%;
+    text-align: center;
+}
+
+.btnStart {
+    margin-left: 100;
+    margin-right: 100;
+
+}
+
+.header1 {
+    font-size: 50;
+    text-align: center;
+    color: #000;
+    font-weight: 800;
+    margin-bottom: 40;
+    width: 100%;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,104 @@
+
+
+<template>
+<Page actionBarHidden="true">
+
+  <FlexboxLayout class="page">
+
+    <StackLayout class="form">
+      <Label text="tindog" class="header" />
+
+
+        <TextField class="input-field1" text="Username" />
+
+
+        <TextField class="input-field2" secure="true" text="password" />
+
+      <Button text="New User? Register" tap="submit" class="btn btn-primary btnLogin" />
+      <Button text="Login Instead" tap="submit" class=" btn-secondary" />
+
+        <Label text="copyright tindog 2020" class="footer" />
+
+    </StackLayout>
+  </FlexboxLayout>
+</Page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+.page {
+  align-items: center;
+  background-color: #f5f5f5;
+  vertical-align: center;
+  height: 100%;
+}
+
+.form {
+  vertical-align: center;
+  width: 100%;
+  margin-left: 10;
+  margin-right: 10;
+  background-color: #f5f5f5;
+}
+
+.header {
+  font-size: 40;
+  text-align: center;
+  color: #000;
+  font-weight: 600;
+  margin-bottom: 40;
+}
+
+.input-field1 {
+  margin-top: 10;
+  margin-bottom: -5;
+  box-sizing: border-box;
+  color: #dddddd;
+  color: #dddddd;
+  z-index: 2;
+  font-size: 22;
+  background-color: #fff;
+  border-top-left-radius: 6;
+  border-top-right-radius: 6;
+  border-bottom-left-radius: 6;
+  border-bottom-right-radius:
+    6;
+  border-block-end-color: gray;
+}
+
+.input-field2 {
+  margin-bottom: 30;
+  color: #dddddd;
+  color: #dddddd;
+  z-index: 2;
+  font-size: 22;
+  background-color: #fff;
+  border-bottom-left-radius:
+    6;
+  border-bottom-right-radius: 6;
+  border-top-left-radius: 6;
+  border-top-right-radius: 6;
+}
+
+
+
+.btnLogin {
+  background-color: #fbd46d;
+  margin-left: 10;
+  margin-right: 10;
+  font-size: 16;
+}
+
+.footer {
+  text-align: center;
+  margin-top: 1;
+  color: gray;
+}
+</style>
